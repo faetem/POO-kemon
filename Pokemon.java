293e60c61849d8b105fcaf0d194ceed35ce82acc
faetem <tourefaetemh@gmail.com>
2025-03-05 11:27:47 +0100

    nouvelles modifs

---
+++ Pokemon.java
@@ -0,0 +1,11 @@
+public class Pokemon {
+    String nom; 
+    String type; 
+    int pointsDeVie;
+
+    Pokemon(String nom, String type, int pointsDeVie) {
+        this.nom  = nom; 
+        this.type = type; 
+        this.pointsDeVie = pointsDeVie;
+    }
+}
